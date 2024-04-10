3468f6d08c36321ad92fbd865671c643ddc3379f
mudegiarnold <136020298+mudegiarnold@users.noreply.github.com>
2024-04-10 13:10:32 +0000

    Added a condition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,9 @@ let myVariable = "Bob";
 myVariable= "arnold";
 /*Everything inbetween is a comment.*/
 // This is a comment
+let icecream = "chocolate"
+if (icecream === "chocolate") {
+    alert("Yah, I love chocolate icecream!");
+} else {
+alert("Awwww, but chocolate is my favourite..");
+}
